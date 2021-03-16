bccb6597c82db6699e30f994aff055b31b1e023f
Michel Marechal <michelm.marechal@gmail.com>
2021-03-16 16:49:45 -0300

    add files
    Add files

---
+++ public/app.js
@@ -0,0 +1,47 @@
+
+const urlEmployees = "http://localhost:3000/employees",
+      urlRoles = "http://localhost:3000/roles",
+      cargos = [],
+      empregados = [];
+
+fetchData(urlEmployees, empregados);
+fetchData(urlRoles, cargos);
+
+function fetchData(url, array) {
+      fetch(url).then(data => {return data.json()}).then(data => {data.map((dado) => {array.push(dado)})});
+};
+
+function montaTabela(empregados, cargos) {
+      const tabela = document.querySelector('tbody');
+      empregados.map((empregado) => {
+            tabela.innerHTML += 
+            `<tr>
+                  <td>${empregado.id}</td>
+                  <td>${empregado.name}</td>
+                  <td>${cargos[empregado.role_id - 1].name}</td>
+                  <td>R$ ${empregado.salary}</td>
+            </tr>`      
+      });
+}
+
+function organizaPorSalario(array) {
+      return array.sort((a, b) => (b.salary > b.salary) ? 1 : -1);
+}
+
+function organizaPorNome(array) {
+      return array.sort((a, b) => (a.name > b.name) ? 1 : -1);
+}
+
+let checarDadosCarregados = setInterval(() => {
+      if (cargos[0] != undefined) {
+            clearInterval(checarDadosCarregados);
+
+            montaTabela(empregados, cargos, '.tbody');
+            document.querySelector('.preloader').classList.add('display-none');
+      
+            const filtroHTML = document.querySelector('.checkbox')
+            cargos.forEach((cargo) => {
+                  filtroHTML.innerHTML += `<div><label><input type="radio" name="role" class="cargo-label" id="${cargo.id}"> ${cargo.name}</label></div>`;
+            });
+      }
+}, 50);
